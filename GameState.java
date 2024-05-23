8c8a6a0410425adafab0f4e9347b7ffe281b870f
injilaadi <injilaadil@gmail.com>
2024-05-22 23:23:07 -0700

    new class

---
+++ GameState.java
@@ -0,0 +1,15 @@
+import java.util.List;
+//import greenfoot subsitute?
+
+public class GameState extends State{
+    int frames = 0;
+    public GameState(GameWorld gameW){
+        super(gameW);
+    }
+    public void onSet(){
+        //create character and main screen
+    }
+    public void onAct(){
+        //add game choices and if statements
+    }
+}
